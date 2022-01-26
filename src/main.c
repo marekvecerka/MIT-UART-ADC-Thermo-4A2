bafe0c03803de796c948f852a741ba0752af4c07
marekvecerka <vec38035@spseol.cz>
2022-01-26 13:05:44 +0100

    teplota

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "stm8s.h"
 #include "milis.h"
-#include "stm8s_adc2.h"
+
 #include "spse_stm8.h"
 /*#include "delay.h"*/
 #include <stdio.h>
@@ -56,6 +56,8 @@ int main(void)
 {
     uint32_t time = 0;
     uint16_t ADCx;
+    uint16_t napeti;
+    uint16_t teplota;
 
     setup();
 
@@ -65,7 +67,11 @@ int main(void)
             LED_REVERSE; 
             time = milis();
             ADCx = ADC_get(ADC2_CHANNEL_4);
-            printf("%ld %d\r\n", time, ADCx);
+            napeti = ((uint32_t)3300 * ADCx + 512) / 1024;
+            teplota = ((uint32_t)33000 * ADCx - 4096000 + 99840) / 19668;
+            printf("ADCx = %d ; U = %dmV ; T = %d.%d°C \r\n", 
+                    ADCx, napeti, teplota/10, teplota%10);
+
         }
 
         /*LED_REVERSE; */
